b671f7981c9317b4b9a33e07cb59393502480b89
nithyakalathil <nithya913k@gmail.com>
2024-06-13 02:30:17 +0530

    created

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.post("/search",(req,res)=>{
     })
 })
 
+app.post("/delete",(req,res)=>{
+    let input=req.body
+    bookingmodel.findByIdAndDelete(input._id).then(
+        (response)=>{res.json({status:"Success"})}
+    )
+})
 
 app.listen(8081,()=>{
     console.log("server started")
